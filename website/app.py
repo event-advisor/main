e09dcccdae2439ef0dbeb872730049eb9c2a00f2
souless94 <cl.wk@hotmail.com>
2019-04-21 15:49:50 +0800

    add links to different pages of the eventbrite search.

--- website/app.py
+++ website/app.py
@@ -8,17 +8,23 @@ eventData = eventbriteData.EventbriteData()
 def index():
     return render_template("index.html")
 
-@app.route("/predict", methods =['POST'])
+@app.route("/predict/", methods =['GET'])
 def predict():
-    searchtxt = request.form['search']
+    # user
+    page = request.args.get("p",default = 1, type = str)
+    searchtxt = request.args.get("q",default = 1, type = str)
+    pageUrl = request.url
 
-    items = eventData.getData(searchtxt)
+    #backend
+    items = eventData.getData(page,searchtxt)
     eventNames = eventData.getEventName(items,searchtxt)
     eventUrls =  eventData.getEventUrl(items,searchtxt)
     eventImageUrls = eventData.getEventImageUrl(items,searchtxt)
     eventDescriptions = eventData.getEventDescription(items,searchtxt)
     eventLocations = eventData.getEventlocation(items,searchtxt)
     eventTimes = eventData.getEventTime(items,searchtxt)
+
+    pageUrls= [ pageUrl[:-1] + str(i) for i in range(1,10)]
 
     return render_template("result.html",
     searchinput = searchtxt,
@@ -27,7 +33,8 @@ def predict():
     eventImageUrls=eventImageUrls,
     eventDescriptions=eventDescriptions,
     eventLocations = eventLocations,
-    eventTimes = eventTimes)
+    eventTimes = eventTimes,
+    pageUrls = pageUrls)
 
 if __name__ == "__main__":
     app.run()
