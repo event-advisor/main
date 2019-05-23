cc541b4c4964d07d06a6f8b5bbd52e6146556dd0
souless94 <cl.wk@hotmail.com>
2019-05-23 17:54:43 +0800

    try letting selenium wait for page to load

--- DataCollection/peatixData.py
+++ DataCollection/peatixData.py
@@ -1,5 +1,9 @@
 from requests import get
-from selenium import webdriver
+from selenium import webdriver 
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 
 class peatixData:
@@ -30,7 +34,12 @@ class peatixData:
 
     def getData(self,page,searchtxt):
         theUrl = self.getDataUrl(page,searchtxt)
-        print(theUrl,"-------------------")
+        timeout = 20
+        try:
+            WebDriverWait(self._driver, timeout).until(EC.presence_of_element_located((By.CLASS_NAME, "event-thumb ng-scope")))
+        except TimeoutException:
+            print("Timed out waiting for page to load")
+            self._driver.quit()
         self._driver.get(theUrl)
         html_soup = BeautifulSoup(self._driver.page_source,'html.parser')
         event_containers = html_soup.find_all("li",class_="event-thumb ng-scope")
