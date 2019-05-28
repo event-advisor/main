0d17cb5039bdc3b1cb24463e34e36a53b121be60
souless94 <cl.wk@hotmail.com>
2019-05-28 12:19:29 +0800

    update web design, sort events by date and time

--- DataCollection/meetupData.py
+++ DataCollection/meetupData.py
@@ -1,8 +1,14 @@
 from requests import get
+from datetime import datetime
 import os
 class MeetUpData:
     
     def __init__(self):
+        # f = open(r"C:\Users\wen kai\Downloads\y4s2\event-advisor\DataCollection\token.txt","r")
+        # credentials = f.read()
+        # f.close()
+        # txt_arr = credentials.split("\n")
+        # self._token = txt_arr[1]
         self._token = os.environ.get('meetup-token')
         self._url = "https://api.meetup.com/find/upcoming_events/"
         self.category_dict = {"Arts": "Arts", 
@@ -27,23 +33,24 @@ class MeetUpData:
         if (meetupsearchtxt is not None):
             searchtxt = meetupsearchtxt
         payload = { "text":searchtxt,
+                    "order": "time",
                    "access_token" : self._token,
                    "page": "50"}
         response = get(self._url,params=payload).json().get("events")
         return response
-    def getEventName(self,items,searchtxt):
+    def getEventName(self,items):
         names =[]
         for i in range(len(items)):
-            name = items[i].get("name")
+            name = "Meetup " + items[i].get("name")
             names.append(name)
         return names
-    def getEventUrl(self,items,searchtxt):
+    def getEventUrl(self,items):
         urls = []
         for i in range(len(items)):
             url = items[i].get("link")
             urls.append(url)
         return urls
-    def getEventlocation(self,items,searchtxt):
+    def getEventlocation(self,items):
         eventLocations=[]
         for i in range(len(items)):
             if ( items[i].get('venue') is not None):
@@ -54,9 +61,19 @@ class MeetUpData:
             else:
                 eventLocations.append("None")
         return eventLocations
-    def getEventTime(self,items,searchtxt):
+    def getEventTime(self,items):
         eventTimes=[]
         for i in range(len(items)):
-            eventStart = items[i].get("local_date")+" " + items[i].get("local_time")
-            eventTimes.append(eventStart)
+            date =  items[i].get("local_date")
+            date_time = date + " " +items[i].get("local_time")
+            start_time = datetime.strptime(date_time, "%Y-%m-%d %H:%M") # Start Time
+            start_time = start_time.strftime("%H:%M")+":00"
+            eventTimes.append(start_time)
         return eventTimes
+
+    def getEventDate(self,items):
+        eventDates=[]
+        for i in range(len(items)):
+            eventStart = items[i].get("local_date")
+            eventDates.append(eventStart)
+        return eventDates
